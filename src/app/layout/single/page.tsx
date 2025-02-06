ec351955318bb577cad02e31c153f69cee4cfd9f
zakzakst <e1433104@gmai.com>
2025-02-07 00:09:00 +0900

    修正

---
+++ src/app/layout/single/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="p-6 mx-auto sm:max-w-screen-sm">
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default Page;
