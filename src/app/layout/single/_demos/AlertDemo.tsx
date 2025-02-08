6b222c272d3611a7fb58fb9644fa5ff4fef221fb
zakzakst <e1433104@gmai.com>
2025-02-08 15:38:37 +0900

    修正

---
+++ src/app/layout/single/_demos/AlertDemo.tsx
@@ -0,0 +1,24 @@
+import { AlertCircle } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import DesignHeading from "@/app/layout/_components/DesignHeading";
+import DesignBox from "@/app/layout/_components/DesignBox";
+
+const AlertDemo = () => {
+  return (
+    <section>
+      <DesignHeading tag="h4">アラート</DesignHeading>
+      <DesignBox className="mt-4">
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>アラートタイトル</AlertTitle>
+          <AlertDescription>
+            アラートの内容（タイトルのフォントのweightが英語用で設定されていた。そのため、日本語だとデザイン的のメリハリが消えてしまっていた。この辺は適宜調整必要と感じた）
+          </AlertDescription>
+        </Alert>
+      </DesignBox>
+    </section>
+  );
+};
+
+export default AlertDemo;
--- src/app/layout/single/page.tsx
+++ src/app/layout/single/page.tsx
@@ -1,10 +1,13 @@
 import AccordionDemo from "./_demos/AccordionDemo";
+import AlertDemo from "./_demos/AlertDemo";
 
 const Page = () => {
   return (
-    <>
+    <div className="grid grid-cols-1 gap-8">
       <AccordionDemo />
-    </>
+      <AlertDemo />
+      {/* https://ui.shadcn.com/docs/components/alert-dialog */}
+    </div>
   );
 };
 
