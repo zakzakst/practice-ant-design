dc9dfe2b29e8bb7f0977f598a28377548211dde4
zakzakst <e1433104@gmai.com>
2025-02-08 18:11:55 +0900

    修正

--- src/app/layout/single/page.tsx
+++ src/app/layout/single/page.tsx
@@ -5,19 +5,51 @@ import AvatarDemo from "./_demos/AvatarDemo";
 import BadgeDemo from "./_demos/BadgeDemo";
 import BreadcrumbDemo from "./_demos/BreadcrumbDemo";
 import ButtonDemo from "./_demos/ButtonDemo";
+import CardDemo from "./_demos/CardDemo";
 
 const Page = () => {
   return (
     <div className="grid grid-cols-1 gap-8">
       <AccordionDemo />
       <AlertDemo />
-      {/* https://ui.shadcn.com/docs/components/alert-dialog */}
       <AspectRatioDemo />
       <AvatarDemo />
       <BadgeDemo />
       <BreadcrumbDemo />
       <ButtonDemo />
-      {/* https://ui.shadcn.com/docs/components/calendar */}
+      <CardDemo />
+      {/* 簡単 */}
+      {/* https://ui.shadcn.com/docs/components/pagination */}
+      {/* https://ui.shadcn.com/docs/components/progress */}
+      {/* https://ui.shadcn.com/docs/components/scroll-area */}
+      {/* https://ui.shadcn.com/docs/components/separator */}
+      {/* https://ui.shadcn.com/docs/components/table */}
+      {/* https://ui.shadcn.com/docs/components/toggle */}
+      {/* https://ui.shadcn.com/docs/components/toggle-group */}
+      {/* https://ui.shadcn.com/docs/components/tooltip */}
+      {/* TODO: */}
+      {/* https://ui.shadcn.com/docs/components/alert-dialog */}
+      {/* https://ui.shadcn.com/docs/components/checkbox */}
+      {/* https://ui.shadcn.com/docs/components/dialog */}
+      {/* https://ui.shadcn.com/docs/components/drawer */}
+      {/* https://ui.shadcn.com/docs/components/dropdown-menu */}
+      {/* https://ui.shadcn.com/docs/components/form */}
+      {/* https://ui.shadcn.com/docs/components/hover-card */}
+      {/* https://ui.shadcn.com/docs/components/input */}
+      {/* https://ui.shadcn.com/docs/components/label */}
+      {/* https://ui.shadcn.com/docs/components/menubar */}
+      {/* https://ui.shadcn.com/docs/components/navigation-menu */}
+      {/* https://ui.shadcn.com/docs/components/popover */}
+      {/* https://ui.shadcn.com/docs/components/radio-group */}
+      {/* https://ui.shadcn.com/docs/components/select */}
+      {/* https://ui.shadcn.com/docs/components/sheet */}
+      {/* https://ui.shadcn.com/docs/components/sidebar */}
+      {/* https://ui.shadcn.com/docs/components/slider */}
+      {/* https://ui.shadcn.com/docs/components/sonner */}
+      {/* https://ui.shadcn.com/docs/components/switch */}
+      {/* https://ui.shadcn.com/docs/components/tabs */}
+      {/* https://ui.shadcn.com/docs/components/textarea */}
+      {/* https://ui.shadcn.com/docs/components/toast */}
     </div>
   );
 };
