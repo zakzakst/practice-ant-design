4c1a8d87f3a39a74f3727374233523c74494ad23
zakzakst <e1433104@gmai.com>
2025-02-09 22:46:45 +0900

    修正

--- src/app/layout/single/form2/page.tsx
+++ src/app/layout/single/form2/page.tsx
@@ -1,25 +1,45 @@
 "use client";
 
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Switch } from "@/components/ui/switch";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 
 type FormValues = {
   username: string;
   profile: string;
   accept: boolean;
+  switch: boolean;
+  favorite: string;
+  color: string;
 };
 
-const DefaultValues: FormValues = { username: "", profile: "", accept: false };
+const DefaultValues: FormValues = {
+  username: "",
+  profile: "",
+  accept: false,
+  switch: false,
+  favorite: "",
+  color: "",
+};
 
 const Page = () => {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm<FormValues>({
     defaultValues: DefaultValues,
@@ -63,31 +83,118 @@ const Page = () => {
         </div>
 
         <div className="mt-4">
-          <Label htmlFor="accept">同意</Label>
-
-          <Checkbox
-            id="accept"
-            {...register("accept", {
+          <Controller
+            name="accept"
+            control={control}
+            rules={{
               required: "同意が必要です",
-            })}
-            onCheckedChange={(e) =>
-              setValues({ ...values, accept: e as boolean })
-            }
+              // validate: (value) => value || "同意が必要です",
+            }}
+            render={({ field }) => (
+              <Checkbox
+                id="accept"
+                checked={field.value}
+                onCheckedChange={(e) => field.onChange(e)}
+              />
+            )}
           />
+          <Label htmlFor="accept">同意する</Label>
           {errors.accept && (
             <p className="text-destructive">{errors.accept.message}</p>
           )}
         </div>
 
         <div className="mt-4">
+          <Controller
+            name="switch"
+            control={control}
+            rules={{
+              required: "同意が必要です",
+            }}
+            render={({ field }) => (
+              <Switch
+                id="switch"
+                checked={field.value}
+                onCheckedChange={(e) => field.onChange(e)}
+              />
+            )}
+          />
+          <Label htmlFor="switch">同意する</Label>
+          {errors.switch && (
+            <p className="text-destructive">{errors.switch.message}</p>
+          )}
+        </div>
+
+        <div className="mt-4">
+          <Controller
+            name="favorite"
+            control={control}
+            rules={{
+              required: "オプションを選択してください",
+            }}
+            render={({ field }) => (
+              <RadioGroup
+                // defaultValue="option-one"
+                value={field.value}
+                onValueChange={(e) => field.onChange(e)}
+              >
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="option-one" id="option-one" />
+                  <Label htmlFor="option-one">Option One</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="option-two" id="option-two" />
+                  <Label htmlFor="option-two">Option Two</Label>
+                </div>
+              </RadioGroup>
+            )}
+          />
+          {errors.favorite && (
+            <p className="text-destructive">{errors.favorite.message}</p>
+          )}
+        </div>
+
+        <div className="mt-4">
+          <Controller
+            name="color"
+            control={control}
+            rules={{
+              required: "色を選択してください",
+            }}
+            render={({ field }) => (
+              <Select
+                defaultValue="dark"
+                value={field.value}
+                onValueChange={(e) => field.onChange(e)}
+              >
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="色を選択してください" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="red" disabled>
+                    Red
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          />
+          {errors.color && (
+            <p className="text-destructive">{errors.color.message}</p>
+          )}
+        </div>
+
+        {/* TODO: APIで項目を取得するパターンのセレクトボックス */}
+
+        <div className="mt-4">
           <Button type="submit" disabled={Object.keys(errors).length > 0}>
-            Submit
+            送信
           </Button>
         </div>
 
         <div className="mt-4">
           <pre>{JSON.stringify(values, null, 2)}</pre>
-          {/* <pre>{JSON.stringify(errors)}</pre> */}
         </div>
       </form>
     </div>
