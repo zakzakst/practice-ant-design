b59167b9024eb490ea32334e9a743556ee5b8c84
zakzakst <e1433104@gmai.com>
2025-02-12 02:12:59 +0900

    修正

--- src/app/tools/_components/ListInput.tsx
+++ src/app/tools/_components/ListInput.tsx
@@ -22,45 +22,54 @@ const ListInput = ({ items, onChange }: Props) => {
   };
 
   const deleteItem = (index: number) => {
-    onChange(items.filter((item, i) => i !== index));
+    onChange(items.filter((_, i) => i !== index));
   };
 
   return (
     <div>
       {items.length > 0 && (
-        <ul>
+        <ul className="list-disc list-inside grid gap-0 mb-2">
           {items.map((item, index) => (
             <li key={index}>
-              {item}
-              <Button
-                size="icon"
-                variant="ghost"
-                onClick={() => deleteItem(index)}
-              >
-                <X className="h-4 w-4" />
-              </Button>
+              <span className="inline-flex items-center gap-2">
+                {item}
+                <Button
+                  size="icon"
+                  variant="ghost"
+                  onClick={() => deleteItem(index)}
+                  className="w-6 h-6"
+                >
+                  <X className="h-4 w-4" />
+                </Button>
+              </span>
             </li>
           ))}
         </ul>
       )}
       {isShowInput ? (
         <>
-          <Input
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <Button size="sm" disabled={!inputValue} onClick={() => addItem()}>
-            リストに追加
-          </Button>
-          <Button size="sm" onClick={() => setIsShowInput(false)}>
-            入力欄を隠す
-          </Button>
+          <div>
+            <Input
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </div>
+          <div className="mt-2 flex gap-2">
+            <Button size="sm" disabled={!inputValue} onClick={() => addItem()}>
+              リストに追加
+            </Button>
+            <Button size="sm" onClick={() => setIsShowInput(false)}>
+              入力欄を隠す
+            </Button>
+          </div>
         </>
       ) : (
-        <Button size="sm" onClick={() => setIsShowInput(true)}>
-          入力欄を表示
-        </Button>
+        <div>
+          <Button size="sm" onClick={() => setIsShowInput(true)}>
+            入力欄を表示
+          </Button>
+        </div>
       )}
     </div>
   );
