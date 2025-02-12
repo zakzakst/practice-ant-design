11d70fbdcb81a3cdf6cd00c8b9338de5f0028b7c
zakzakst <e1433104@gmai.com>
2025-02-13 08:46:30 +0900

    Updates

--- _next/static/chunks/app/sample-base2/page2/page-ff3545d47b76cb65.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3816,2710,7932,6222,2822,6523,1072,9332,6517,9942,183,3229,980,7257,415,7481],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/sample-base3/page3/page-4d9a187cb83500c7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6222,2710,7932,3229,980,7257,1072,3816,9332,6517,2822,6523,9942,183,415,7481],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
