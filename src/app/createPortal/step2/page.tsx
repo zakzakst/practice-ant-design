0ea777f39f9066aabdd8095f0e61a0dde03ecd0d
zakzakst <e1433104@gmai.com>
2025-02-22 18:04:06 +0900

    修正

--- src/app/createPortal/step2/page.tsx
+++ src/app/createPortal/step2/page.tsx
@@ -1,9 +1,10 @@
+"use client";
 // NOTE: 上手くできなかった。詰まってしまったのでエラーが出るが一旦ここで終了
+// ⇒useModalを利用するファイル（このファイル）にuse clientを付けてなかったのが原因だった
 import { useModal } from "../context/ModalContext";
 
 const Page = () => {
-  console.error("useModal", useModal);
-  const { openModal } = useModal();
+  const { openModal, closeModal } = useModal();
 
   const handleOpenModal = () => {
     openModal(
@@ -18,7 +19,7 @@ const Page = () => {
     <div>
       <h1>Next.jsのグローバルモーダル管理例</h1>
       <button onClick={handleOpenModal}>モーダルを開く</button>
-      {/* <button onClick={closeModal}>モーダルを閉じる</button> */}
+      <button onClick={closeModal}>モーダルを閉じる</button>
     </div>
   );
 };
