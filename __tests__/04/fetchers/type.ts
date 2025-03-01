f017cc0ea5b83341a147ba68146b000316fbb6bf
zakzakst <e1433104@gmai.com>
2025-03-01 20:28:09 +0900

    修正

---
+++ __tests__/04/fetchers/type.ts
@@ -0,0 +1,15 @@
+export type HttpError = {
+  err: { message: string };
+};
+
+export type Article = {
+  id: string;
+  createdAt: string;
+  tags: string[];
+  title: string;
+  body: string;
+};
+
+export type Articles = {
+  articles: Article[];
+};
