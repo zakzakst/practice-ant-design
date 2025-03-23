74b9e29a16ae3b4ca01292a84e98eaa80d906a33
zakzakst <e1433104@gmai.com>
2025-03-23 18:47:27 +0900

    修正

--- src/app/react-flow/page.tsx
+++ src/app/react-flow/page.tsx
@@ -1,21 +1,22 @@
 "use client";
 import {
   ReactFlow,
-  // Controls,
+  Controls,
   // Background,
   // BackgroundVariant,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 // import { Nodes, Edges, NodeTypes } from "./data";
-import { Nodes, Edges, CustomNode, CustomEdge } from "./data2";
+// import { Nodes, Edges, CustomNode, CustomEdge } from "./data2";
+import { MyNodes, MyEdges, myNodeTypes } from "./MyData";
 
-const nodeTypes = {
-  custom: CustomNode, // keyにNodeType名, valueにカスタムノードコンポーネント
-};
+// const nodeTypes = {
+//   custom: CustomNode, // keyにNodeType名, valueにカスタムノードコンポーネント
+// };
 
-const edgeTypes = {
-  custom: CustomEdge, // keyにEdgeType名, valueにカスタムエッジコンポーネント
-};
+// const edgeTypes = {
+//   custom: CustomEdge, // keyにEdgeType名, valueにカスタムエッジコンポーネント
+// };
 
 // const Page = () => {
 //   return (
@@ -33,17 +34,37 @@ const edgeTypes = {
 //   );
 // };
 
+// const Page = () => {
+//   return (
+//     <div>
+//       <p>react flow</p>
+//       <div style={{ width: "50vw", height: "50vh" }}>
+//         <ReactFlow
+//           nodeTypes={nodeTypes}
+//           edgeTypes={edgeTypes}
+//           nodes={Nodes}
+//           edges={Edges}
+//         />
+//       </div>
+//       <p>react flow</p>
+//     </div>
+//   );
+// };
+
 const Page = () => {
   return (
     <div>
       <p>react flow</p>
       <div style={{ width: "50vw", height: "50vh" }}>
         <ReactFlow
-          nodeTypes={nodeTypes}
-          edgeTypes={edgeTypes}
-          nodes={Nodes}
-          edges={Edges}
-        />
+          nodeTypes={myNodeTypes}
+          // edgeTypes={edgeTypes}
+          nodes={MyNodes}
+          edges={MyEdges}
+          fitView
+        >
+          <Controls />
+        </ReactFlow>
       </div>
       <p>react flow</p>
     </div>
