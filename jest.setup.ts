01ffd23c3b75472e8c591bc83da9975f4285021f
zakzakst <e1433104@gmai.com>
2025-04-05 17:14:53 +0900

    修正

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
-import { server } from "@/mocks/server";
+// import { server } from "@/mocks/server";
 
-beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+// beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+// afterEach(() => server.resetHandlers());
+// afterAll(() => server.close());
