bc3d9ac0a2096fc81b783c4bd7535c96a707f45e
zakzakst <e1433104@gmai.com>
2025-08-17 19:13:00 +0900

    fix

--- src/app/swr/practice/SwrSample3.tsx
+++ src/app/swr/practice/SwrSample3.tsx
@@ -1,9 +1,124 @@
 "use client";
+import useSWRMutation from "swr/mutation";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, Controller } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { sleep } from "./utils";
+
+const postTodoFormSchema = z.object({
+  todo: z.string().min(1, "TODOは入力必須項目です"),
+  completed: z.boolean(),
+});
+
+type PostTodoFormValue = z.infer<typeof postTodoFormSchema>;
+
+type PostTodoRequest = PostTodoFormValue & {
+  userId: number;
+};
+
+type Todo = {
+  id: number;
+  todo: string;
+  completed: boolean;
+  userId: number;
+};
+
+type PostTodoResponse = Todo;
+
+class SwrError extends Error {
+  data?: unknown;
+  status?: number;
+}
+
+const IsUseErrorMock = false;
+const IsUseTodoMock = false;
+
+const PostTodoResponseMock: PostTodoResponse = {
+  id: 1,
+  todo: "掃除する",
+  completed: false,
+  userId: 5,
+};
+
+const postTodoFetcher = async (
+  url: string,
+  { arg }: { arg: PostTodoRequest }
+): Promise<PostTodoResponse> => {
+  if (IsUseTodoMock) {
+    sleep(1000);
+    return PostTodoResponseMock;
+  }
+  const res = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(arg),
+  });
+  if (!res.ok) {
+    const error = new Error() as SwrError;
+    error.data = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+  return res.json();
+};
+
+const usePostTodo = () => {
+  const url = IsUseErrorMock
+    ? "https://dummyjson.com/http/404/404エラーのレスポンスメッセージ"
+    : "https://dummyjson.com/todos/add";
+  const { trigger, error, isMutating } = useSWRMutation(url, postTodoFetcher);
+  return {
+    trigger,
+    error,
+    isMutating,
+  };
+};
 
 export const SwrSample = () => {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors: formErrors },
+  } = useForm<PostTodoFormValue>({
+    resolver: zodResolver(postTodoFormSchema),
+    defaultValues: {
+      todo: "",
+      completed: false,
+    },
+  });
+  const { trigger, error } = usePostTodo();
+  const onSubmit = async (value: PostTodoFormValue) => {
+    console.log(value);
+    const res = await trigger({
+      ...value,
+      userId: 1,
+    });
+    console.log(res);
+  };
   return (
     <div>
-      <h1>fetch/axios GET/POST/PUT/DELETE Todo</h1>
+      <h1>fetch POST Todo</h1>
+      <Input {...register("todo")} />
+      {formErrors.todo && <p>{formErrors.todo.message}</p>}
+      <div className="flex items-center gap-2">
+        完了：
+        <Controller
+          name="completed"
+          control={control}
+          render={({ field }) => (
+            <Checkbox
+              checked={field.value}
+              onCheckedChange={(val) => field.onChange(val)}
+            />
+          )}
+        />
+      </div>
+      <Button onClick={handleSubmit(onSubmit)}>送信</Button>
+      {error && <p>{error.data.message}</p>}
     </div>
   );
 };
