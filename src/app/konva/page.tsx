a08fecaa82db74409d8dc9d6b22fb7ebb1c81b6e
zakzakst <e1433104@gmai.com>
2025-09-05 22:41:43 +0900

    fix

--- src/app/konva/page.tsx
+++ src/app/konva/page.tsx
@@ -2,7 +2,9 @@
 // import { Konva } from "./practice/Konva2";
 // import { Konva } from "./practice/Konva3";
 // import { KonvaPractice as Konva } from "./practice/Konva4";
-import { KonvaPractice as Konva } from "./practice/Konva5";
+// import { KonvaPractice as Konva } from "./practice/Konva5";
+// import { KonvaPractice as Konva } from "./practice/Konva6";
+import { KonvaPractice as Konva } from "./practice/Konva7";
 
 const Page = () => {
   return (
