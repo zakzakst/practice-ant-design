c09eb2b65068c8fa7bced87246c3027e317ffd87
zakzakst <e1433104@gmai.com>
2025-09-23 16:45:51 +0900

    fix

--- src/app/react-hook-form4/page.tsx
+++ src/app/react-hook-form4/page.tsx
@@ -3,7 +3,8 @@
 // import { Form } from "./practice/Form3";
 // import { Form } from "./practice/Form4";
 // import { Form } from "./practice/Form5";
-import { Form } from "./practice/Form6";
+// import { Form } from "./practice/Form6";
+import { Form } from "./practice/Form7";
 
 const Page = () => {
   return (
