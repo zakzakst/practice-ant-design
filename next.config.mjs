4268c1de2c6810c8501eb9ae627fd1724245798c
zakzakst <e1433104@gmai.com>
2025-09-28 16:10:47 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.BASE_PATH,
-  output: "export",
+  // output: "export",
   // SSGではnext/imageが利用できない。今回はunoptimizedで割愛
   images: {
     unoptimized: true,
