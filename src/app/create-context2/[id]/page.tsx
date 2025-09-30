42b32e67921fd6b6bd520214d92fbcb8876693fa
zakzakst <e1433104@gmai.com>
2025-09-30 20:48:14 +0900

    fix

--- src/app/create-context2/[id]/page.tsx
+++ src/app/create-context2/[id]/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import { useEffect } from "react";
 import { useParams } from "next/navigation";
+import { useUser } from "../context/UserContext";
 
 const Page = () => {
   const params = useParams<{ id: string }>();
   const id = params.id;
+  const { user, setUser } = useUser();
 
-  return <div>{id}</div>;
+  useEffect(() => {
+    setUser({
+      id,
+      name: "名前",
+    });
+  }, [id, setUser]);
+
+  return <div>{JSON.stringify(user)}</div>;
 };
 
 export default Page;
--- src/app/create-context2/layout.tsx
+++ src/app/create-context2/layout.tsx
@@ -1,9 +1,11 @@
+import { UserProvider } from "./context/UserContext";
+
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return <UserProvider>{children}</UserProvider>;
 };
 
 export default Layout;
