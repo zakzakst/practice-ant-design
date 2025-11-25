60e5e5f303b844b029f8da08f0353e5da6d7e21d
zakzakst <e1433104@gmail.com>
2025-11-25 19:57:54 +0900

    fix

--- src/app/dnd-kit/page.tsx
+++ src/app/dnd-kit/page.tsx
@@ -1,5 +1,15 @@
+'use client'
+
+import { DndKit } from "./parts/dnd-kit1"
+
+import { DndContext } from "@dnd-kit/core";
+
 const Page = () => {
-  return <div>page</div>
+  return <div>
+    <DndContext>
+      <DndKit />
+    </DndContext>
+  </div>
 }
 
 export default Page
---
+++ src/app/dnd-kit/parts/_dnd-kit.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export const DndKit = () => {
+  return <div>DndKit</div>
+}
