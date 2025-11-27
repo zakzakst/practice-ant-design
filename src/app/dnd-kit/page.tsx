6262933bf2cacc11a2c8d4c55925c5b4d5067908
zakzakst <e1433104@gmail.com>
2025-11-27 22:08:35 +0900

    fix

--- src/app/dnd-kit/page.tsx
+++ src/app/dnd-kit/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 
-import { DndKit } from "./parts/dnd-kit1"
+// import { DndKit } from "./parts/dnd-kit1"
+import { DndKit } from "./parts/dnd-kit2"
 
-import { DndContext } from "@dnd-kit/core";
+// import { DndContext } from "@dnd-kit/core";
 
 const Page = () => {
   return <div>
-    <DndContext>
+    {/* <DndContext>
       <DndKit />
-    </DndContext>
+    </DndContext> */}
+    <DndKit />
   </div>
 }
 
--- src/app/dnd-kit/parts/dnd-kit2.tsx
+++ src/app/dnd-kit/parts/dnd-kit2.tsx
@@ -1,5 +1,64 @@
 'use client'
 
+import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
+import { useState, useId } from "react";
+
+const DraggableItem = () => {
+  const id = useId();
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id,
+  })
+
+  const style = {
+    width: 100,
+    height: 100,
+    backgroundColor: "skyblue",
+    borderRadius: 8,
+    transform: transform
+      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      : undefined,
+  }
+
+  return <div ref={setNodeRef} style={style} {...listeners} {...attributes} />
+}
+
+const DroppableArea = ({ isDropped }: { isDropped: boolean }) => {
+  // const id = useId();
+  const { setNodeRef, isOver } = useDroppable({
+    id: 'drop-zone',
+  });
+
+  const style = {
+    width: 200,
+    height: 200,
+    border: "2px dashed #666",
+    borderRadius: 12,
+    backgroundColor: isDropped ? "gold" : isOver ? "#f0f0f0" : "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  return (
+    <div ref={setNodeRef} style={style}>
+      Drop Here
+    </div>
+  );
+}
+
 export const DndKit = () => {
-  return <div>DndKit</div>
+  const [isDropped, setIsDropped] = useState(false);
+
+  return <div>
+    <DndContext
+      onDragEnd={({ over }) => {
+        if (over && over.id === 'drop-zone') {
+          setIsDropped(true);
+        }
+      }}
+    >
+      <DraggableItem />
+      <DroppableArea isDropped={isDropped} />
+    </DndContext>
+  </div>
 }
