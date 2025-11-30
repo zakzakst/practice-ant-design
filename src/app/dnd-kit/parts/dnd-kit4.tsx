59a50f5bd032bee91bb2515130f83f7c39986178
zakzakst <e1433104@gmail.com>
2025-11-30 23:22:12 +0900

    fix

--- src/app/dnd-kit/parts/dnd-kit4.tsx
+++ src/app/dnd-kit/parts/dnd-kit4.tsx
@@ -1,5 +1,27 @@
 'use client'
 
+import {
+  DndContext,
+  DragOverlay,
+  closestCenter,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+  arrayMove,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { useState } from "react";
+
+type Item = {
+  id: string;
+  label: string;
+};
+
 export const DndKit = () => {
   return <div>DndKit</div>
 }
