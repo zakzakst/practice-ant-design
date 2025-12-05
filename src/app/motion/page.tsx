deb9898a857cf1cf7562d953cdc1e10b5f4601b5
zakzakst <e1433104@gmail.com>
2025-12-06 03:09:36 +0900

    fix

--- src/app/motion/page.tsx
+++ src/app/motion/page.tsx
@@ -1,7 +1,8 @@
 // import { Motion } from "./parts/motion1";
 // import { Motion } from "./parts/motion2";
 // import { Motion } from "./parts/motion3";
-import { Motion } from "./parts/motion4";
+// import { Motion } from "./parts/motion4";
+import { Motion } from "./parts/motion5";
 
 const Page = () => {
   return (
