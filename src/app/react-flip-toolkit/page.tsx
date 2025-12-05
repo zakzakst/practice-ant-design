e8c43584d72173ae4abab7577709b2f7cff34834
zakzakst <e1433104@gmail.com>
2025-12-05 20:35:32 +0900

    fix

--- src/app/react-flip-toolkit/page.tsx
+++ src/app/react-flip-toolkit/page.tsx
@@ -1,6 +1,7 @@
 // import { ReactFlipToolkit } from "./parts/react-flip-toolkit1";
 // import { ReactFlipToolkit } from "./parts/react-flip-toolkit2";
-import { ReactFlipToolkit } from "./parts/react-flip-toolkit3";
+// import { ReactFlipToolkit } from "./parts/react-flip-toolkit3";
+import { ReactFlipToolkit } from "./parts/react-flip-toolkit4";
 
 const Page = () => {
   return <div>
